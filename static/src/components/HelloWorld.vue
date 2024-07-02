7f7a62f7564aad7601eb3b5d89a4183d2f9c9ed6
Ikekwem Daniel <youngkhito@gmail.com>
2024-07-02 08:58:42 +0100

    first commit

---
+++ static/src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "Loading..."
+    };
+  },
+  mounted() {
+    fetch("http://127.0.0.1:8000")
+      .then(response => response.json())
+      .then(data => {
+        this.message = data.message;
+      });
+  }
+};
+</script>
+
+<style scoped>
+h1 {
+  font-family: Arial, sans-serif;
+}
+</style>
