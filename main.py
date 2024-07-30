61d3558e2dfd5b5f75065775e22b8b2900758612
Grant du Toit <32957150+GrantduToit@users.noreply.github.com>
2024-07-30 03:08:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ stack = Stack()
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8080"],  # Allow specific origins
+    allow_origins=["*"],  # Allow specific origins
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -23,6 +23,18 @@ class LEDToggle(BaseModel):
     led_num: int
     state: int
 
+class LEDVALToggle(BaseModel):
+    led_num: int
+    val: int
+
+class MOTORToggle(BaseModel):
+    motor_num: int
+    state: int
+
+class CAMToggle(BaseModel):
+    cam_num: int
+    state: int
+
 def sensor_data():   
     global stack
     while True:
@@ -30,7 +42,7 @@ def sensor_data():
         # stack.get_bme_data() returns all the BME280 data
         retv = json.dumps({'current':stack.get_current_sensor_data()})
         time.sleep(1)
-        print({"success": True,'sensordata': retv})
+        #print({"success": True,'sensordata': retv})
 
 sensor_thread = threading.Thread(target=sensor_data)
 sensor_thread.daemon = True
@@ -50,6 +62,32 @@ def handle_led_toggle(data: LEDToggle):
     LED_BOARD = 1
     stack.switch(LED_BOARD, led_num+1, state)
     return {"success": True, "message": f"LED Channel {led_num} toggled to {state}"}
+
+@app.post("/led_brightness")
+def handle_led_brightness(data: LEDVALToggle):
+    led_num = data.led_num
+    val = data.val
+    LED_PIN = 0
+    stack.set_pwm_out(led_num+LED_PIN, val)
+    return {"success": True, "message": f"LED Channel increased to {val}"}
+    
+@app.post("/motor_toggle")
+def handle_motor_toggle(data: MOTORToggle):  
+    motor_num = data.motor_num
+    state = data.state  
+    MOTOR_BOARD = 2
+    stack.switch(MOTOR_BOARD, motor_num+1, state)
+    return {"success": True, "message": f"Motor Channel {motor_num} toggled to {state}"}
+
+
+@app.post("/cam_toggle")
+def handle_cam_toggle(data: CAMToggle):   
+    cam_num = data.cam_num
+    state = data.state   
+    CAM_BOARD = 0
+    stack.switch(CAM_BOARD, cam_num+1, state)
+    return {"success": True, "message": f"CAMERA Channel {cam_num} toggled to {state}"}
+
 
 @app.get("/sensor_data")
 def get_sensor_data():
